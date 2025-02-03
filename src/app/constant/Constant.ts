e0c6fa756a9619d160574f89efe3bcd32c6c4dc6
AhteshamUddowla <ahteshamiiuc@gmail.com>
2025-02-03 09:41:34 +0600

    Const file added

---
+++ src/app/constant/Constant.ts
@@ -0,0 +1,10 @@
+export const Constant = {
+    API_URL: "https://freeapi.miniprojectideas.com/api/amazon/",
+    PRODUCT_API_METHOD: {
+        GET_ALL_PRODUCT: "GetAllProducts",
+        GET_PRODUCT: "GetProductById?id=",
+        UPDATE_PRODUCT: "UpdateProduct",
+        DELETE_PRODUCT: "DeleteProductById?id=",
+        CREATE_PRODUCT: "CreateProduct"
+    }
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IAPIResponseModel } from '../models/interfaces/api-response';
 import { Product } from '../models/classes/product-class';
+import { Constant } from '../constant/Constant';
 
 @Injectable({
   providedIn: 'root'
@@ -11,22 +12,22 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getAllProducts(){
-    return this.http.get<IAPIResponseModel>("https://freeapi.miniprojectideas.com/api/amazon/GetAllProducts")
+    return this.http.get<IAPIResponseModel>(Constant.API_URL+Constant.PRODUCT_API_METHOD.GET_ALL_PRODUCT)
   }
 
   getProduct(id:number){
-    return this.http.get<IAPIResponseModel>("https://freeapi.miniprojectideas.com/api/amazon/GetProductById?id="+id)
+    return this.http.get<IAPIResponseModel>(Constant.API_URL+Constant.PRODUCT_API_METHOD.GET_PRODUCT+id)
   }
 
   updateProduct(product:Product){
-    return this.http.post<IAPIResponseModel>("https://freeapi.miniprojectideas.com/api/amazon/UpdateProduct", product)
+    return this.http.post<IAPIResponseModel>(Constant.API_URL+Constant.PRODUCT_API_METHOD.UPDATE_PRODUCT, product)
   }
 
   deleteProduct(id:number){
-    return this.http.delete<IAPIResponseModel>("https://freeapi.miniprojectideas.com/api/amazon/DeleteProductById?id="+id)
+    return this.http.delete<IAPIResponseModel>(Constant.API_URL+Constant.PRODUCT_API_METHOD.DELETE_PRODUCT+id)
   }
 
   createProduct(product:Product){
-    return this.http.post<IAPIResponseModel>("https://freeapi.miniprojectideas.com/api/amazon/CreateProduct", product)
+    return this.http.post<IAPIResponseModel>(Constant.API_URL+Constant.PRODUCT_API_METHOD.CREATE_PRODUCT, product)
   }
 }
